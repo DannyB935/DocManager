1b2979eafefa5955393b9f98ea6dc4a62ba97796
DannyB935 <daniel.bustos935@hotmail.com>
2025-08-21 16:25:53 -0600

    feat(web-react): Initializes the React-based frontend
    Sets up the basic structure for a React frontend using Next.js,
    including core dependencies, configuration files, and initial
    component structure, along with necessary tooling for linting and
    styling. It provides the foundation for building out the
    application's user interface.
    Also the HeroUI components library was installed

---
+++ frontend-react/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { Inter } from 'next/font/google'
+import "./globals.css";
+
+import {Providers} from "./providers";
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'DocManager',
+  description: 'DocManager system developed on React and Next.js',
+}
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${inter.className} light`}>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ frontend-react/src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {Button} from '@heroui/button'; 
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='font-bold'>Hola</h1>
+      Probando
+      <Button color='secondary'>Click me</Button>
+    </>
+  );
+}
